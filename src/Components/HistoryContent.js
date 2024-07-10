9af6ff6a6b3290caad7c242a51aec5950bd9701f
swamy3697 <swamyp2002@gmail.com>
2024-07-10 14:30:59 +0530

    updated to the blue theme

--- src/Components/HistoryContent.js
+++ src/Components/HistoryContent.js
@@ -5,7 +5,10 @@ import '../Components/HistoryContainer.css'
 function HistoryContainer(){
     return (<>
     <div className="history_container">
-        <img className="nohistory_image" src={noHistory} alt="no History"/>
+    
+  <h1>
+    No History
+  </h1>
     </div>
     </>);
 }
